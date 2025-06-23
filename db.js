1fd78046983de28620cd75c5b7a4b4b0a1f7bec3
Guilherme Negretto <guilherme.negretto@fullsoft.com.br>
2025-06-23 11:28:33 -0300

    feature: conecta com banco PostgreSQL via variável DB_URL

--- db.js
+++ db.js
@@ -1,12 +1,19 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
 
-  database: 'postgres',
-  password: 'pgadmin',
-  port: 5432,
+//   database: 'postgres',
+//   password: 'pgadmin',
+//   port: 5432,
+// });
+
+const pool = new Pool({
+  connectionString: process.env.DB_URL, // <- essa variável estará configurada no Render
+  ssl: {
+    rejectUnauthorized: false // necessário para conexão segura no Render
+  }
 });
 
 module.exports = {
