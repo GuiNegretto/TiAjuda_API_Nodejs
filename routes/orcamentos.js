f9a540c9afb8adb707ead4bbcd6666d0709d0120
Guilherme Negretto <guilherme.negretto@fullsoft.com.br>
2025-06-24 18:53:33 -0300

    fix: require send mail

--- routes/orcamentos.js
+++ routes/orcamentos.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db'); // ou o caminho do seu módulo de conexão com o banco
+const { sendEmail } = require('../services/emailService'); // Importa a função sendEmail do novo serviço!
+
 
 // GET: Lista todos os orçamentos
 router.get('/', async (req, res) => {
